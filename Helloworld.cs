4fbb77f5873aa99b580b15b9aa81e7d244f58077
harshbhadoria011 <harshbhadoria011@gmail.com>
2024-03-29 18:36:27 +0530

    Changes in Readme and added Helloworld.cs

---
+++ Helloworld.cs
@@ -0,0 +1,7 @@
+namespace Helloworld{
+    public class Program{
+        public static void Main(string[] args){
+            Console.WriteLine("Hello Git");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Learning Basic Github and Git commands.
 <br>
 Author: Harsh Bhadoria
+<br>
+This is my first change in this project from VS Code
